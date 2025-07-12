4121bbb7065b86f4d9e0e2992bfd1d77caff71d8
Hito <134347015+apple-computer-machintosh@users.noreply.github.com>
2025-07-12 14:21:07 +0900

    feat: WIP heroセクションの実装途中

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,17 @@
+import Container from "../shared/Container";
+
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section className="relative pt-32 lg:pt-36">
+      {""}
+      <Container>
+        <div
+          className="absolute -left-6 lg:top-28 w-24 h-24 router-90 skew-x-12
+          rounded-3xl bg-gradient-to-r from-blue-600 to-violet-500"
+        ></div>
+      </Container>
+    </section>
+  );
 };
 
 export default Hero;
